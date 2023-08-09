2d3163d08cddac27e88dfefc40fd817fd4ed46dc
ProCodeRed <yadav.vipinkumar@outlook.com>
2023-08-10 01:18:05 +0530

    password hash added

--- server/model/UserSchema.js
+++ server/model/UserSchema.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -26,6 +27,18 @@ const userSchema = new mongoose.Schema({
         required: true,
     },
     })
+
+
+
+// hashing passoword
+
+userSchema.pre("save", async function(next){
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password, 13);
+        this.cpassword = await bcrypt.hash(this.cpassword, 13);
+    }
+    next();
+})
 
 const User = mongoose.model('user', userSchema);
 
--- server/routers/auth.js
+++ server/routers/auth.js
@@ -21,15 +21,18 @@ router.post('/register', async (req, res) => {
 
         if (userexist) {
             return res.status(422).json({ error: "This email already has been registered" })
-        }
+        }else if(password != cpassword){
+            return res.status(422).json({ error: "Oops! password mismached" })
+        }else{
 
-        const users = new User({ name, email, phone, work, password, cpassword });
-        const userData = await users.save();
-
-        // console.log(userData)
-
-        if (userData) {
-            res.status(201).json({ message: "user registered successfully" })
+            const users = new User({ name, email, phone, work, password, cpassword });
+            const userData = await users.save();
+    
+            // console.log(userData)
+    
+            if (userData) {
+                res.status(201).json({ message: "user registered successfully" })
+            }
         }
 
     } catch (error) {
